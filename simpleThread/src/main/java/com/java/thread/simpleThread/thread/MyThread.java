0a102850289c12f2ce45217c865a7ecd5bd83a95
sathish <sathishkumar_s@persistent.com>
2019-09-04 18:15:16 +0530

    print stmnt added

--- simpleThread/src/main/java/com/java/thread/simpleThread/thread/MyThread.java
+++ simpleThread/src/main/java/com/java/thread/simpleThread/thread/MyThread.java
@@ -3,6 +3,7 @@ package com.java.thread.simpleThread.thread;
 public class MyThread implements Runnable {
 
 	public MyThread() {
+		System.out.println("constructor  : MyThread");
 	}
 
 	public void run() {
